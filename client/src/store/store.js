2e8b2205112ecff22e43cdb69014c3500a832caa
Jose Chavarria <josechavarria@me.com>
2022-07-23 12:27:25 +0200

    Redux dev tools off

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,8 +2,12 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/reducers";
 import thunk from "redux-thunk";
 
+// const store = createStore(
+//     rootReducer,
+//     compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
 const store = createStore(
     rootReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    compose(applyMiddleware(thunk)));
 
 export default store;
